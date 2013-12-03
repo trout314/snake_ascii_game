65639aff869899bdf830abb4db5bdfed895ff364
Vadas Gintautas <vadasg@calico.chatham.local>
2013-12-03 11:26:45 -0500

    moved TO DOs to issues

--- snake.py
+++ snake.py
@@ -84,8 +84,6 @@ snakes = [
     [{"x":5, "y":3}, {"x":6, "y":3}, {"x":7, "y":3}, {"x":8, "y":3}]
 ]
 
-# TO DO: Fill mushroom position list with num_mushrooms
-# number of randomly placed mushrooms
 mushrooms = [{"x":12, "y":7},{"x": 23, "y":5},{"x":68, "y":12},{"x":12, "y":15}]
     
 #-----------------------------------------------------------------------------
@@ -134,10 +132,6 @@ while True:
     # Draw bullets
     draw_bullets()
 
-    # TO DO: draw_snakes
-    # TO DO: draw_spiders
-    # TO DO: draw_mushrooms
-
     # draw screen and then wait
     scr.refresh()
     time.sleep(time_delay)
@@ -156,15 +150,5 @@ while True:
     # perform other movements
     move_bullets()
 
-    # TO DO (BIG): move_snakes
-    # TO DO (BIG): move_spiders
-    
     # resolve consequences of collisions.
     # NOTE: some of these may be best handled by putting code elsewhere
-    
-    #TO DO (BIG): Bullet hits snake segment (remove segment, now two snakes)
-    #TO DO: Bullet hits spider (spider removed)
-    #TO DO: Bullet hits mushroom (mushroom removed)
-    #TO DO: Spider hits player (game over)
-    #TO DO: Snake segment hits player (game over)
-    #TO DO: Snake segment touches last playable row (game over)
